a81f9f3ba0470b9531f5504da54a46df8c5d0b2c
Tac Phan <tintert1703@gmail.com>
2026-07-14 14:56:34 +0700

    style: set address column header width to narrow down layout width

--- src/views/staff-list-view.tsx
+++ src/views/staff-list-view.tsx
@@ -260,7 +260,7 @@ export function StaffListView() {
                                     <th className="px-6 py-4">{UI_TEXT.staff.thName}</th>
                                     <th className="px-6 py-4">{UI_TEXT.staff.thContact}</th>
                                     <th className="w-24 px-6 py-4 text-center whitespace-nowrap">{UI_TEXT.staff.thGender}</th>
-                                    <th className="px-6 py-4">{UI_TEXT.staff.thAddress}</th>
+                                    <th className="w-24 px-6 py-4">{UI_TEXT.staff.thAddress}</th>
                                     <th className="w-24 px-6 py-4 text-center">{UI_TEXT.staff.thRole}</th>
                                     <th className="px-6 py-4">{UI_TEXT.staff.thSystem}</th>
                                     <th className="w-28 px-6 py-4 text-center">{UI_TEXT.staff.thStatus}</th>
